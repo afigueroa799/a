2db019e9dc51f73f12187567acb23e68e73ce0f0
afigueroa799 <afigueroaruano@gmail.com>
2025-03-01 21:20:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Asegurar que el script se ejecute cuando la página cargue
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("boton-verde").addEventListener("click", function() {
+        alert("Sabía que dirías que si amorrr 😍");
+    });
+
+    document.getElementById("boton-rojo").addEventListener("click", function() {
+        alert("No tienes escapatoria de mi bbsota");
+    });
+});
